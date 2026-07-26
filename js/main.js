90ced922de0874bd95a34d744d4ecf397f0d8f92
Anl_23 <cctv.anl22@gmail.com>
2026-07-26 23:24:09 +0300

    Highlight current menu link on page load
    Add active class to the current menu link based on the page.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    let currentPage = window.location.pathname.split("/").pop();
+
+    document.querySelectorAll(".menu-link").forEach(link => {
+
+        let linkPage = link.getAttribute("href").split("/").pop();
+
+        if (currentPage === linkPage) {
+            link.classList.add("active");
+        }
+
+    });
+
+});
