38beb097b262cfc27fce6c5bcd1f75b8b6daf628
Anl_23 <cctv.anl22@gmail.com>
2026-08-04 15:35:25 +0300

    Implement theme toggle for light and dark modes
    Add theme toggle functionality to switch between light and dark modes.

---
+++ js/script.js
@@ -0,0 +1,57 @@
+function openLine(element){
+
+    let content = element.nextElementSibling;
+
+    content.classList.toggle("active");
+
+}
+
+
+// Hamburger menü
+
+const menuToggle = document.querySelector(".menu-toggle");
+const sidebar = document.querySelector(".sidebar");
+
+
+if(menuToggle && sidebar){
+
+    menuToggle.addEventListener("click", ()=>{
+
+        sidebar.classList.toggle("active");
+
+    });
+
+}
+const themeButtons = document.querySelectorAll(".theme-toggle");
+
+
+themeButtons.forEach(button => {
+
+    button.addEventListener("click", ()=>{
+
+
+        document.body.classList.toggle("dark");
+
+
+        if(document.body.classList.contains("dark")){
+
+            localStorage.setItem("theme","dark");
+
+        }else{
+
+            localStorage.setItem("theme","light");
+
+        }
+
+
+    });
+
+});
+
+
+
+if(localStorage.getItem("theme") === "dark"){
+
+    document.body.classList.add("dark");
+
+}
